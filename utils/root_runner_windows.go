85880bba4cbdcbff6dcb2d4b2a248e3933897b06
GuillermoAmbar <guillermoap19994@gmail.com>
2026-01-13 13:59:31 -0500

    use go-winres to add app certificate to support uac

--- utils/root_runner_windows.go
+++ utils/root_runner_windows.go
@@ -6,15 +6,22 @@ package utils
 import (
 	"log"
 	"os"
-	"os/exec"
 	"strings"
 
 	"golang.org/x/sys/windows"
 )
 
 func IsRoot() bool {
-	adminCheck := exec.Command("net", "session")
-	return adminCheck.Run() == nil
+
+	// adminCheck := exec.Command("net", "session")
+	// return adminCheck.Run() == nil
+
+	var hToken windows.Token
+	if err := windows.OpenProcessToken(windows.CurrentProcess(), windows.TOKEN_QUERY, &hToken); err != nil {
+		return false
+	}
+	defer hToken.Close()
+	return hToken.IsElevated()
 }
 
 func RelaunchAsRoot() error {
@@ -38,6 +45,17 @@ func RelaunchAsRoot() error {
 	var showCmd int32 = windows.SW_HIDE
 
 	err := windows.ShellExecute(0, shellCommandPtr, executablePathPtr, joinedAppArgsPtr, runnningPathPtr, showCmd)
+
+	// verb := syscall.StringToUTF16Ptr("runas")
+	// file := syscall.StringToUTF16Ptr(exe)
+	// params := syscall.StringToUTF16Ptr(strings.Join(args, " "))
+	// dir := syscall.StringToUTF16Ptr(workDir)
+	// show := int32(1) // SW_SHOWNORMAL
+	// r, _, err := syscall.NewLazyDLL("shell32.dll").NewProc("ShellExecuteW").Call( 0, uintptr(unsafe.Pointer(verb)), uintptr(unsafe.Pointer(file)), uintptr(unsafe.Pointer(params)), uintptr(unsafe.Pointer(dir)), uintptr(show), )
+	// // ShellExecute returns >32 on success
+	// if r <= 32 {
+	// 	return fmt.Errorf("ShellExecuteW failed: %v (code=%d)", err, r)
+	//  }
 
 	return err
 }
